f219b32540e6462aae6081c680832639871ad7c4
ahmedezar <73957153+ahmedezar@users.noreply.github.com>
2023-02-17 15:58:02 +0100

    userflow

---
+++ src/userflow/dtos/userflow.dto.ts
@@ -0,0 +1,14 @@
+import { type } from "os";
+import { flowData } from "src/flow/dtos/flow.dto";
+
+export class CreateUserflowDTO {
+    companyId: string;
+    userId: string;
+    offerId: string;
+    memberId: string;
+    userFlow: flow[];
+}
+export type flow = flowData & {
+    status: string;
+    statusDate: string;
+}
---
+++ src/userflow/userflow.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import {  flow }from './dtos/userflow.dto';
+
+export type UserflowDocument = Userflow & Document;
+
+@Schema()
+export class Userflow {
+    @Prop({
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      })
+    userId: string;
+
+  
+    @Prop()
+    userFlow: flow[];
+
+   
+
+
+}
+
+export const UserflowSchema = SchemaFactory.createForClass(Userflow);
